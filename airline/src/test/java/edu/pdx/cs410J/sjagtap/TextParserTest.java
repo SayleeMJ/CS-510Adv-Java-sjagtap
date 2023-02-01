bd91d2ac8190425e1f1826b1d02588f3367ac505
SayleeMJ <jagtapsaylee.2020@gmail.com>
2023-02-01 11:10:42 -0800

    Added test case for TextParserTest

--- airline/src/test/java/edu/pdx/cs410J/sjagtap/TextParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/sjagtap/TextParserTest.java
@@ -3,8 +3,7 @@ package edu.pdx.cs410J.sjagtap;
 import edu.pdx.cs410J.ParserException;
 import org.junit.jupiter.api.Test;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -14,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class TextParserTest {
 
   @Test
-  void validTextFileCanBeParsed() throws ParserException {
+  void validTextFileCanBeParsed()  {
     try {
       InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
       assertThat(resource, notNullValue());
@@ -34,7 +33,44 @@ public class TextParserTest {
     TextParser parser = new TextParser(new InputStreamReader(resource));
     assertThrows(ParserException.class, parser::parse);
   }
+  @Test
+  void readingCreatedFile() throws FileNotFoundException, ParserException {
+    String fileName = "D:\\testing.txt";
+    Airline airline;
+    Reader r = new FileReader(fileName);
+    TextParser textParser = new TextParser(r);
+    airline = textParser.parse();
+    assertThat(airline.getName(), equalTo("Indigo"));
+  }
 
+  @Test
+  void readingCreatedFileForExistingAirlineName() {
+    try{
+      String fileName = "D:\\testing.txt";
+      Airline airline;
+      Reader r = new FileReader(fileName);
+      TextParser textParser = new TextParser(r);
+      airline = textParser.parse();
+      String existingAirlineName = airline.getName();
+      String newAirlineName = "Indigo";
+      assertThat(existingAirlineName,equalTo(newAirlineName));
+    } catch (Exception e){
+      assertThat(e.getMessage(),equalTo("Airline name is different!"));
+    }
+  }
 
-
+@Test
+  void readingCreatedFileForNotExistingAirlineName() {
+    try{
+      String fileName = "D:\\testing.txt";
+      Airline airline;
+      Reader r = new FileReader(fileName);
+      TextParser textParser = new TextParser(r);
+      airline = textParser.parse();
+      String existingAirlineName = airline.getName();
+      String newAirlineName = "Emirates";
+    } catch (Exception e){
+      assertThat(e.getMessage(),equalTo("Airline name is different!"));
+    }
+  }
 }
