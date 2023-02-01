c54af8633e2bc5267ec2aca205aea713c8e3898c
SayleeMJ <jagtapsaylee.2020@gmail.com>
2023-02-01 11:40:06 -0800

    Added test case for TextParserTest

--- airline/src/test/java/edu/pdx/cs410J/sjagtap/TextParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/sjagtap/TextParserTest.java
@@ -11,46 +11,49 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TextParserTest {
 
-  @Test
-  void validTextFileCanBeParsed()  {
-    try {
-      InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
-      assertThat(resource, notNullValue());
+    @Test
+    void validTextFileCanBeParsed() {
+        try {
+            InputStream resource = getClass().getResourceAsStream("valid-airline.txt");
+            assertThat(resource, notNullValue());
 
-      TextParser parser = new TextParser(new InputStreamReader(resource));
-      Airline airline = parser.parse();
-    } catch (Exception e){
-      assertThat(e.getMessage(), equalTo("Missing flight number"));
+            TextParser parser = new TextParser(new InputStreamReader(resource));
+            Airline airline = parser.parse();
+        } catch (Exception e) {
+            assertThat(e.getMessage(), equalTo("Missing flight number"));
+        }
     }
-  }
 
-  @Test
-  void invalidTextFileThrowsParserException() {
-    InputStream resource = getClass().getResourceAsStream("empty-airline.txt");
-    assertThat(resource, notNullValue());
+    @Test
+    void invalidTextFileThrowsParserException() {
+        InputStream resource = getClass().getResourceAsStream("empty-airline.txt");
+        assertThat(resource, notNullValue());
 
-    TextParser parser = new TextParser(new InputStreamReader(resource));
-    assertThrows(ParserException.class, parser::parse);
-  }
-  @Test
-  void readingCreatedFile() throws FileNotFoundException, ParserException {
-    String fileName = "D:\\testing.txt";
-    Airline airline;
-    Reader r = new FileReader(fileName);
-    TextParser textParser = new TextParser(r);
-    airline = textParser.parse();
-    assertThat(airline.getName(), equalTo("Indigo"));
-  }
+        TextParser parser = new TextParser(new InputStreamReader(resource));
+        assertThrows(ParserException.class, parser::parse);
+    }
 
-  @Test
-  void readingCreatedFileForExistingAirlineName() throws FileNotFoundException, ParserException {
-      String fileName = "D:\\testing.txt";
-      Airline airline;
-      Reader r = new FileReader(fileName);
-      TextParser textParser = new TextParser(r);
-      airline = textParser.parse();
-      String existingAirlineName = airline.getName();
-      String newAirlineName = "Indigo";
-      assertThat(existingAirlineName,equalTo(newAirlineName));
-  }
+    @Test
+    void readingCreatedFile() throws FileNotFoundException, ParserException {
+        InputStream resource = getClass().getResourceAsStream("testing.txt");
+        assertThat(resource, notNullValue());
+
+        TextParser parser = new TextParser(new InputStreamReader(resource));
+        Airline airline;
+        airline = parser.parse();
+        assertThat(airline.getName(), equalTo("Indigo"));
+    }
+
+    @Test
+    void readingCreatedFileForExistingAirlineName() throws FileNotFoundException, ParserException {
+        InputStream resource = getClass().getResourceAsStream("testing.txt");
+        assertThat(resource, notNullValue());
+
+        TextParser parser = new TextParser(new InputStreamReader(resource));
+        Airline airline;
+        airline = parser.parse();
+        String existingAirlineName = airline.getName();
+        String newAirlineName = "Indigo";
+        assertThat(existingAirlineName, equalTo(newAirlineName));
+    }
 }
