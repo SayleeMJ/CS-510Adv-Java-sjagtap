ac38f8e0860d56e049f60696bc4064a455d2ffb6
SayleeMJ <jagtapsaylee.2020@gmail.com>
2023-03-05 16:55:28 -0800

    Added some integration tests

--- airline-web/src/it/java/edu/pdx/cs410J/sjagtap/AirlineRestClientIT.java
+++ airline-web/src/it/java/edu/pdx/cs410J/sjagtap/AirlineRestClientIT.java
@@ -1,63 +1,109 @@
-//package edu.pdx.cs410J.sjagtap;
-//
-//import edu.pdx.cs410J.ParserException;
-//import edu.pdx.cs410J.web.HttpRequestHelper;
-//import org.junit.jupiter.api.MethodOrderer.MethodName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.TestMethodOrder;
-//
-//import java.io.IOException;
-//import java.net.HttpURLConnection;
-//import java.util.Map;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//
-///**
-// * Integration test that tests the REST calls made by {@link AirlineRestClient}
-// */
-//@TestMethodOrder(MethodName.class)
-//class AirlineRestClientIT {
-//  private static final String HOSTNAME = "localhost";
-//  private static final String PORT = System.getProperty("http.port", "8080");
-//
-//  private AirlineRestClient newAirlineRestClient() {
-////    int port = Integer.parseInt(PORT);
-//    return new AirlineRestClient(HOSTNAME, port);
-//  }
-//
-//  @Test
-//  void test0RemoveAllDictionaryEntries() throws IOException {
-//    AirlineRestClient client = newAirlineRestClient();
-//    client.removeAllDictionaryEntries();
-//  }
-//
-//  @Test
-//  void test1EmptyServerContainsNoDictionaryEntries() throws IOException, ParserException {
-//    AirlineRestClient client = newAirlineRestClient();
-//    Map<String, String> dictionary = client.getAllDictionaryEntries();
-//    assertThat(dictionary.size(), equalTo(0));
-//  }
-//
-//  @Test
-//  void test2DefineOneWord() throws IOException, ParserException {
-//    AirlineRestClient client = newAirlineRestClient();
-//    String testWord = "TEST WORD";
-//    String testDefinition = "TEST DEFINITION";
-//    client.addDictionaryEntry(testWord, testDefinition);
-//
-//    String definition = client.getDefinition(testWord);
-//    assertThat(definition, equalTo(testDefinition));
-//  }
-//
-//  @Test
-//  void test4EmptyWordThrowsException() {
-//    AirlineRestClient client = newAirlineRestClient();
-//    String emptyString = "";
-//
-//    HttpRequestHelper.RestException ex =
-//      assertThrows(HttpRequestHelper.RestException.class, () -> client.addDictionaryEntry(emptyString, emptyString));
-//    assertThat(ex.getHttpStatusCode(), equalTo(HttpURLConnection.HTTP_PRECON_FAILED));
-//    assertThat(ex.getMessage(), equalTo(Messages.missingRequiredParameter(AirlineServlet.WORD_PARAMETER)));
-//  }}
+package edu.pdx.cs410J.sjagtap;
+
+import edu.pdx.cs410J.ParserException;
+import edu.pdx.cs410J.web.HttpRequestHelper;
+import org.junit.jupiter.api.MethodOrderer.MethodName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Integration test that tests the REST calls made by {@link AirlineRestClient}
+ */
+@TestMethodOrder(MethodName.class)
+class AirlineRestClientIT {
+    private static final String HOSTNAME = "localhost";
+    private static final String PORT = System.getProperty("http.port", "8080");
+
+    private AirlineRestClient newAirlineRestClient() {
+        int port = Integer.parseInt(PORT);
+        return new AirlineRestClient(HOSTNAME, port);
+    }
+
+    @Test
+    void test0RemoveAllDictionaryEntries() throws IOException {
+        AirlineRestClient client = newAirlineRestClient();
+        client.removeAllDictionaryEntries();
+    }
+
+    @Test
+    void test1EmptyServerContainsNoDictionaryEntries() throws IOException, ParserException {
+        AirlineRestClient client = newAirlineRestClient();
+
+        HttpRequestHelper.RestException ex =
+                assertThrows(HttpRequestHelper.RestException.class, () -> client.getFlights("test"));
+        assertThat(ex.getHttpStatusCode(), equalTo(HttpURLConnection.HTTP_NOT_FOUND));
+        assertThat(ex.getMessage(), equalTo(Messages.airlineNotFound("test")));
+    }
+
+    @Test
+    void test1EmptyAirlineParameter() throws IOException, ParserException {
+        AirlineRestClient client = newAirlineRestClient();
+
+        HttpRequestHelper.RestException ex =
+                assertThrows(HttpRequestHelper.RestException.class, () -> client.getFlights(""));
+        assertThat(ex.getHttpStatusCode(), equalTo(HttpURLConnection.HTTP_PRECON_FAILED));
+        assertThat(ex.getMessage(), equalTo(Messages.missingRequiredParameter("airline")));
+    }
+
+    @Test
+    void test2DefineFlight() throws IOException, ParserException {
+        AirlineRestClient client = newAirlineRestClient();
+        String airline = "TEST AIRLINE";
+        String flightNo = "123";
+        String src = "PDX";
+        String departDate = "07/19/2023 1:02 pm";
+        String dest = "ORD";
+        String arriveDate = "07/19/2023 6:22 pm";
+        client.addFlightEntry(airline, flightNo, src, departDate, dest, arriveDate);
+
+        Airline airlineObj = client.getFlights(airline);
+        assertThat(airlineObj.getFlights().size(), equalTo(1));
+        // assert on other fields
+    }
+
+    // try to add flight with invalid depart date format
+
+    // try to add flight with invalid arrive date format
+
+    // try to add flight with empty airline name
+
+    // try to add flight with empty flight number
+
+    // try to add flight with empty source
+
+    // try to add flight with empty destination
+
+    // try to add flight with empty arrive date
+
+    // try to add flight with empty depart date
+
+
+    // try to get flights with empty source but non empty destination
+
+    // try to get flights with empty destination but non empty source
+
+    // both are empty but there are no flights for that airline
+
+    // both are given, but there no flights
+
+    // both are given, only 1 flight matches
+
+    // both are given, only 2 flight matches
+
+    // get flights for a airline where there is only 1 airline
+
+    // get flights for a airline where there are more than 1 airline
+
+
+
+
+
+}
--- airline-web/src/it/java/edu/pdx/cs410J/sjagtap/Project5IT.java
+++ airline-web/src/it/java/edu/pdx/cs410J/sjagtap/Project5IT.java
@@ -1,88 +1,73 @@
-//package edu.pdx.cs410J.sjagtap;
-//
-//import edu.pdx.cs410J.InvokeMainTestCase;
-//import edu.pdx.cs410J.UncaughtExceptionInMain;
-//import edu.pdx.cs410J.web.HttpRequestHelper.RestException;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.TestMethodOrder;
-//
-//import java.io.IOException;
-//import java.net.HttpURLConnection;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.containsString;
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.junit.jupiter.api.Assertions.fail;
-//import static org.junit.jupiter.api.MethodOrderer.MethodName;
-//
-///**
-// * An integration test for {@link Project5} that invokes its main method with
-// * various arguments
-// */
-//@TestMethodOrder(MethodName.class)
-//class Project5IT extends InvokeMainTestCase {
-//    private static final String HOSTNAME = "localhost";
-//    private static final String PORT = System.getProperty("http.port", "8080");
-//
-//    @Test
-//    void test0RemoveAllMappings() throws IOException {
-//      AirlineRestClient client = new AirlineRestClient(HOSTNAME, Integer.parseInt(PORT));
-//      client.removeAllDictionaryEntries();
-//    }
-//
-//    @Test
-//    void test1NoCommandLineArguments() {
-//        MainMethodResult result = invokeMain( Project5.class );
-//        assertThat(result.getTextWrittenToStandardError(), containsString(Project5.MISSING_ARGS));
-//    }
-//
-//    @Test
-//    void test2EmptyServer() {
-//        MainMethodResult result = invokeMain( Project5.class, HOSTNAME, PORT );
-//
-//        assertThat(result.getTextWrittenToStandardError(), equalTo(""));
-//
-//        String out = result.getTextWrittenToStandardOut();
-//        assertThat(out, out, containsString(PrettyPrinter.formatWordCount(0)));
-//    }
-//
-//    @Test
-//    void test3NoDefinitionsThrowsAppointmentBookRestException() {
-//        String word = "WORD";
-//        try {
-//            invokeMain(Project5.class, HOSTNAME, PORT, word);
-//            fail("Should have thrown a RestException");
-//
-//        } catch (UncaughtExceptionInMain ex) {
-//            RestException cause = (RestException) ex.getCause();
-//            assertThat(cause.getHttpStatusCode(), equalTo(HttpURLConnection.HTTP_NOT_FOUND));
-//        }
-//    }
-//
-//    @Test
-//    void test4AddDefinition() {
-//        String word = "WORD";
-//        String definition = "DEFINITION";
-//
-//        MainMethodResult result = invokeMain( Project5.class, HOSTNAME, PORT, word, definition );
-//
-//        assertThat(result.getTextWrittenToStandardError(), equalTo(""));
-//
-//        String out = result.getTextWrittenToStandardOut();
-//        assertThat(out, out, containsString(Messages.definedWordAs(word, definition)));
-//
-//        result = invokeMain( Project5.class, HOSTNAME, PORT, word );
-//
-//        assertThat(result.getTextWrittenToStandardError(), equalTo(""));
-//
-//        out = result.getTextWrittenToStandardOut();
-//        assertThat(out, out, containsString(PrettyPrinter.formatDictionaryEntry(word, definition)));
-//
-//        result = invokeMain( Project5.class, HOSTNAME, PORT );
-//
-//        assertThat(result.getTextWrittenToStandardError(), equalTo(""));
-//
-//        out = result.getTextWrittenToStandardOut();
-//        assertThat(out, out, containsString(PrettyPrinter.formatDictionaryEntry(word, definition)));
-//    }
-//}
+package edu.pdx.cs410J.sjagtap;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import edu.pdx.cs410J.UncaughtExceptionInMain;
+import edu.pdx.cs410J.web.HttpRequestHelper.RestException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.MethodOrderer.MethodName;
+
+/**
+ * An integration test for {@link Project5} that invokes its main method with
+ * various arguments
+ */
+@TestMethodOrder(MethodName.class)
+class Project5IT extends InvokeMainTestCase {
+    private static final String HOSTNAME = "localhost";
+    private static final String PORT = System.getProperty("http.port", "8080");
+
+    @Test
+    void test0RemoveAllMappings() throws IOException {
+      AirlineRestClient client = new AirlineRestClient(HOSTNAME, Integer.parseInt(PORT));
+      client.removeAllDictionaryEntries();
+    }
+
+    @Test
+    void test1NoCommandLineArguments() {
+        MainMethodResult result = invokeMain( Project5.class );
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments. Please provide argument as explained below."));
+    }
+
+    @Test
+    void test2EmptyServer() {
+        MainMethodResult result = invokeMain( Project5.class, "-host", HOSTNAME, "-port", PORT, "airline");
+
+        assertThat(result.getTextWrittenToStandardError(), equalTo("** 404Airline airline not found in the dictionary."));
+
+    }
+
+
+    @Test
+    void test4AddDefinition() {
+
+        MainMethodResult result = invokeMain( Project5.class, "-host", HOSTNAME, "-port", PORT, "airline", "123", "PDX", "07/19/2023", "1:02", "pm", "LAS", "07/19/2023", "6:22", "pm");
+
+        assertThat(result.getTextWrittenToStandardError(), equalTo(""));
+
+        String out = result.getTextWrittenToStandardOut();
+        assertThat(out, out, containsString(Messages.definedAirlineAs("airline", "123")));
+
+        result = invokeMain( Project5.class, "-host",HOSTNAME, "-port", PORT, "airline" );
+
+        assertThat(result.getTextWrittenToStandardError(), equalTo(""));
+
+        Airline airline = new Airline("airline");
+
+        out = result.getTextWrittenToStandardOut();
+
+        assertThat(out, out, containsString("Flight 123 departs 07/19/2023"));
+
+    }
+}
