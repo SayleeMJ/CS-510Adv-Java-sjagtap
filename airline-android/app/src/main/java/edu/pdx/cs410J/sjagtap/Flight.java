b858980b555194b2ebcfc53574915628ce1c0b54
SayleeMJ <jagtapsaylee.2020@gmail.com>
2023-03-15 00:11:11 -0700

    Added dependency and flight class

---
+++ airline-android/app/src/main/java/edu/pdx/cs410J/sjagtap/Flight.java
@@ -0,0 +1,30 @@
+package edu.pdx.cs410J.sjagtap;
+
+import edu.pdx.cs410J.AbstractFlight;
+
+public class Flight extends AbstractFlight {
+    @Override
+    public int getNumber() {
+        return 0;
+    }
+
+    @Override
+    public String getSource() {
+        return null;
+    }
+
+    @Override
+    public String getDepartureString() {
+        return null;
+    }
+
+    @Override
+    public String getDestination() {
+        return null;
+    }
+
+    @Override
+    public String getArrivalString() {
+        return null;
+    }
+}
