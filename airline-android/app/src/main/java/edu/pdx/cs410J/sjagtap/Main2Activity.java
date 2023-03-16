174bc3b584037377d45e468283bdc038b6192f85
SayleeMJ <jagtapsaylee.2020@gmail.com>
2023-03-16 13:48:59 -0700

    Renamed from activity_second.xml

--- airline-android/app/src/main/java/edu/pdx/cs410J/sjagtap/Main2Activity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410J/sjagtap/Main2Activity.java
@@ -8,6 +8,6 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_menu);
     }
 }
